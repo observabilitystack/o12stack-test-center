917351ee68cc385d83ee67bdcd8fdb6e278fe43f
backstreetkiwi <mail@nikolaus-winter.de>
2019-08-27 12:45:50 +0200

    initial version generated by SpringBoot Initializr

---
+++ src/main/java/org/o12stack/o12stack/testcenter/O12stackTestCenterApplication.java
@@ -0,0 +1,13 @@
+package org.o12stack.o12stack.testcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O12stackTestCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O12stackTestCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/o12stack/o12stack/testcenter/O12stackTestCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package org.o12stack.o12stack.testcenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class O12stackTestCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
